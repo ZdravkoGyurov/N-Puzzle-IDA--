ddf640aa6d27ce954833eda35cb66fd000cbfab9
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-10-25 11:14:40 +0300

    added javadoc

--- src/main/java/ida/Node.java
+++ src/main/java/ida/Node.java
@@ -2,6 +2,9 @@ package ida;
 
 import java.util.Arrays;
 
+/**
+ * Class representing a single node/state in the IDA* algorithm
+ */
 public class Node {
     private int[][] boardSnapshot;
     private int heuristic;
@@ -9,6 +12,14 @@ public class Node {
     private int zeroRow;
     private int zeroCol;
 
+    /**
+     * Constructor with parameters
+     * @param boardSnapshot board in this node/state
+     * @param zeroRow row coordinate of the zero/blank tile
+     * @param zeroCol col coordinate of the zero/blank tile
+     * @param heuristic heuristic estimate of the cost to travel to the goal from this board state
+     * @param parent reference to the parent node/previous state
+     */
     public Node(final int[][] boardSnapshot, final int zeroRow, final int zeroCol, final int heuristic, final Node parent) {
         this.boardSnapshot = boardSnapshot;
         this.zeroRow = zeroRow;
@@ -17,22 +28,43 @@ public class Node {
         this.parent = parent;
     }
 
+    /**
+     * Getter for boardSnapshot
+     * @return boardSnapshot - matrix/board in this node/state
+     */
     public int[][] getBoardSnapshot() {
         return boardSnapshot;
     }
 
+    /**
+     * Getter for zeroRow
+     * @return zeroRow - row coordinate of the zero/blank tile
+     */
     public int getZeroRow() {
         return zeroRow;
     }
 
+    /**
+     * Getter for zeroCol
+     * @return zeroCol - col coordinate of the zero/blank tile
+     */
     public int getZeroCol() {
         return zeroCol;
     }
 
+    /**
+     * Getter for the parent node/previous state
+     * @return
+     */
     public Node getParent() {
         return parent;
     }
 
+    /**
+     * Overridden equals method for comparing two nodes' board states, used for determining if IDA* reached the goal
+     * @param o node
+     * @return true if nodes have the same board
+     */
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
@@ -47,10 +79,5 @@ public class Node {
             }
         }
         return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(boardSnapshot);
     }
 }
